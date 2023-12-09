7ab119c34b01c089b8cebd43e595b5836d188c00
Ирина Шипунова <108937167+irina792901@users.noreply.github.com>
2023-12-09 11:51:32 +0300

    Logic

--- app/src/main/java/ru/irinashipunova/mytic_tac_toe/MainActivity.kt
+++ app/src/main/java/ru/irinashipunova/mytic_tac_toe/MainActivity.kt
@@ -2,10 +2,52 @@ package ru.irinashipunova.mytic_tac_toe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.irinashipunova.mytic_tac_toe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MyTickTacToe)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.toNewGame.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.toContinueGame.setOnClickListener {
+            val gameInfo = getInfoAboutLastGame()
+            val intent = Intent(this, GameActivity::class.java).apply {
+                putExtra(EXTRA_TIME, gameInfo.time)
+                putExtra(EXTRA_GAME_FIELD, gameInfo.gameField)
+            }
+            startActivity(intent)
+        }
+
+        binding.toSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        setContentView(binding.root)
     }
+
+    private fun getInfoAboutLastGame() : GameInfo {
+        with(getSharedPreferences("game", MODE_PRIVATE)){
+            val time = getLong("time", 0)
+            val gameField = getString("gameField", "")
+
+            return if (gameField != null) {
+                GameInfo(time, gameField)
+            } else {
+                GameInfo(0,"")
+            }
+        }
+    }
+
+    data class GameInfo(val time: Long, val gameField: String)
 }
